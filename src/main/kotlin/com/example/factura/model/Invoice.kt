322338572a816d499760b101bcce88214033572b
eudematos1 <116099820+eudematos1@users.noreply.github.com>
2023-12-15 11:02:20 -0500

    octavo COMMIT

--- src/main/kotlin/com/example/factura/model/Invoice.kt
+++ src/main/kotlin/com/example/factura/model/Invoice.kt
@@ -18,7 +18,8 @@ class Invoice{
     @Column(updatable = false)
     var id: Long? = null
     var code: String? = null   //description_one en la base de datos
-    var create_at: Date? = null   //address
+    @Column(name="create_at")
+    var createAt: Date? = null   //address
     var total: Double? = null
     @Column(name="client_id")
     var clientId: Long? = null
